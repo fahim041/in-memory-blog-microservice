862ac339de61b67e849fff232e72e913ff4b240b
M0nster92 <fahimalsafi@gmail.com>
2023-05-13 20:48:20 -0600

    handled missing events

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,17 +4,33 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
 
+  events.push(event);
+
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
+
   axios.post("http://localhost:4003/events", event);
+
+  try {
+    axios.post("http://localhost:4002/events", event);
+  } catch (err) {}
 
   res.send({ status: "ok" });
 });
 
+app.get("/events", (req, res) => {
+  res.send(events);
+});
+
 app.listen(4005, () => {
   console.log("Event bus is running on port 4005");
+});
+
+process.on("uncaughtException", function (err) {
+  console.log(err);
 });
--- query/index.js
+++ query/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
@@ -11,8 +12,7 @@ app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
+const handleEvent = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
 
@@ -36,12 +36,28 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvent(type, data);
 
   console.log(posts);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("query service is running on port 4002");
+
+  try {
+    let res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("processing event", event.type);
+
+      handleEvent(event.type, event.data);
+    }
+  } catch (err) {}
 });
